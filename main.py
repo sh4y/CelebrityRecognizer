707635f3312f18534ae2810a39f15cf26691dd75
sh4y <akshay.hariharan@mail.utoronto.ca>
2019-03-20 11:00:21 -0400

    Added SIFT recognition between frames

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import operator
 import matplotlib.pyplot as plt
 
 def get_video_frames(file):
-@@ -15,7 +16,61 @@ def get_video_frames(file):
+    capture = cv2.VideoCapture(file)
+    result, image = capture.read()
+    length = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    frames = {}
+    count = 0
+    frames[count] = image
+    while result:
+        result, image = capture.read()
+        count += 1
         frames[count] = image
     return frames
 
@@ -40,6 +48,23 @@ def compute_potential_matches(des_ref, des_test):
                 potential_matches[tuple(descriptor)] = distances[0][0]
     return potential_matches
 
+def compute_kp_pairs(potential_matches, des_frame1, des_frame2, kp_frame1, kp_frame2):
+    kp_pairs = {}
+    for descriptor in potential_matches:
+        pair = potential_matches[descriptor]
+        for i in range(len(kp_frame1)):
+            flattened_descriptor = np.array(descriptor).flatten()
+            if np.array_equal(np.array(des_frame1[i]), flattened_descriptor):
+                # found descriptor, use i to find keypoint
+                ref_kp = kp_frame1[i]
+                break
+        for i in range(len(kp_frame2)):
+            if np.array_equal(np.array(des_frame2[i]), np.array(pair)):
+                test_kp = kp_frame2[i]
+                break
+        kp_pairs[ref_kp.pt] = test_kp.pt
+    return kp_pairs
+
 def visualize_matches(ref_img, test_img, ref_x, ref_y, test_x, test_y, n=10):
     f, axarr = plt.subplots(1,2)
     axarr[0].axis('off')
@@ -57,15 +82,13 @@ def get_sift_frame_kp_desc(frame):
     return kp, des
 
 frames = get_video_frames('C:\Users\Shay\Documents\CSC420\Project\\test.mp4')
-frame = frames[0]
-plt.imshow(frame)
-plt.show()
 frame1 = frames[0]
 frame2 = frames[1]
 kp1, des1 = get_sift_frame_kp_desc(frame1)
 kp2, des2 = get_sift_frame_kp_desc(frame2)
 des_frame1, des_frame2 = compute_descriptor_dictionaries(kp1, kp2, des1, des2)
 matches_between_frames = compute_potential_matches(des1, des2)
-ref_x, ref_y = map(list,zip(*matches_between_frames.keys()))
-test_x, test_y = map(list,zip(*matches_between_frames.values()))
+kp_pairs = compute_kp_pairs(matches_between_frames, des_frame1, des_frame2, kp1, kp2)
+ref_x, ref_y = map(list,zip(*kp_pairs.keys()))
+test_x, test_y = map(list,zip(*kp_pairs.values()))
 visualize_matches(frame1, frame2, ref_x, ref_y, test_x, test_y)
